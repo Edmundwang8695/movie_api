8b60e88c011813a5ea7540572b7875f2539d5efc
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-08-26 22:26:22 -0700

    Connecting Database to API on Heroku

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ require('./passport');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 
