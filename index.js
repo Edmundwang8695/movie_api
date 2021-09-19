b336a066263b3a3b39bdf2ccdcc3755c906fd89c
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-09-18 23:17:29 -0700

    adding localhost:1234 to allowedorigins

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors= require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com','http://localhost:1234'];
 app.use(cors({
   origin:(origin, callback) =>{
     if(!origin) return callback(null,true);
